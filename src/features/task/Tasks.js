6d9e580d49b7bac05e38dc73b80d989db7e97eb7
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-03-03 20:31:00 +0100

    Update Tasks.js

--- src/features/task/Tasks.js
+++ src/features/task/Tasks.js
@@ -1,13 +1,20 @@
-import React, { useRef } from "react";
+import { selectFilters } from "features/filters/FiltersSlice";
+// import { statusFilter } from "features/filters/StatusFilters";
+import React, { useRef, useEffect} from "react";
 import { useSelector, useDispatch } from "react-redux";
-import {addTask,deleteTask, selectTasks, toggleCompleted } from "./TaskSlice";
+import {addTask,deleteTask, selectTasks, toggleCompleted, fetchTasks } from "./TaskSlice";
 
 
 export function Tasks() {
     const tasks = useSelector(selectTasks);
+    const filters = useSelector(selectFilters);
     const inputRef = useRef(null);
-    console.log("tasks", tasks)
     const dispatch = useDispatch();
+    // console.log("tasks", tasks)
+
+    useEffect(() => {
+        dispatch(fetchTasks());
+    },[dispatch]);
 
     const handleAddTask = () => {
         const taskText = inputRef.current.value;
@@ -18,22 +25,40 @@ export function Tasks() {
         }));
         inputRef.current.value = "";
     }
-    return ( 
+    const getVisibleTasks = (tasks, statusFilter) => {
+        switch (statusFilter) {
+            case "active":
+                return tasks.filter((task) => !task.completed);
+            case "completed":
+                return tasks.filter((task) => task.completed);
+            default:
+                return tasks;
+            
+        }
+    };
+    
+    const statusFilter = filters.status;
+
+    const visibleTasks = getVisibleTasks(tasks.items.statusFilter)
+    return (
         <>
             <h1>Task Manager</h1>
             <input type="text" ref={inputRef} />
             <button onClick={handleAddTask}>Add Task</button>
+            {tasks.isLoading && <p>Loading....</p>}
+            {tasks.error && <p>{tasks.error}</p>}
+            {visibleTasks.length === 0 && !tasks.isLoading && <p>No tasks to show</p>}
             <ul>
-                {tasks.map((task) => (
+                {visibleTasks.map((task) => (
                     <li key={task.id}>
                         <input type="checkbox"
                             checked={task.completed}
                             onChange={() => dispatch(toggleCompleted(task.id))} />
                         {task.text}
-                    <button onClick={()=>dispatch(deleteTask(task.id))}>X</button>
+                        <button onClick={() => dispatch(deleteTask(task.id))}>X</button>
                     </li>
                 ))}
             </ul>
         </>
-     );
+    );
 }
